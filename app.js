28ddff9517ef4fe9b735e4b1f63c8c8731971889
KANHIYA <42318245+kanhiya94@users.noreply.github.com>
2022-04-06 17:48:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+const app = express();
+
+
+passport.use(new BasicStrategy(
+    function (username, password, done) {
+        if (username == "sbdrdn7en8dndd8sm9==" && password == "dccdecdcdecdecdc") {
+            return done(null, "authenticated");
+        } else {
+            return done(null, false);
+        }
+    }
+));
+
+app.use(passport.initialize());
+
+app.get('/users', passport.authenticate('basic', { session: false }), function (req, res) {
+    res.status(200).json({
+        "message": "success",
+        "data": [
+            {
+                id: 1,
+                name: "test1"
+            },
+            {
+                id: 2,
+                name: "test2"
+            },
+            {
+                id: 3,
+                name: "test3"
+            }
+        ]
+    })
+});
+
+app.listen(3000, () => {
+    console.log("server started")
+})
